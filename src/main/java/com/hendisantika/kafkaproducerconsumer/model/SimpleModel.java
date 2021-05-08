729618fea9d9ba349556945240fa53e02ebf8f0c
Hendi Santika <hendisantika@gmail.com>
2021-05-09 06:54:18 +0700

    #3 - Add SimpleModel Model

---
+++ src/main/java/com/hendisantika/kafkaproducerconsumer/model/SimpleModel.java
@@ -0,0 +1,22 @@
+package com.hendisantika.kafkaproducerconsumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kafka-producer-consumer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/21
+ * Time: 06.53
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleModel {
+    private String field1;
+    private String field2;
+}
