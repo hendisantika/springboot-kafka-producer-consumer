24ac239c2fc1f1d4a74b43c14d833f633ea9b97c
Hendi Santika <hendisantika@gmail.com>
2021-05-09 06:57:20 +0700

    #6 - Add producerFactory in KafkaConfig

--- src/main/java/com/hendisantika/kafkaproducerconsumer/config/KafkaConfig.java
+++ src/main/java/com/hendisantika/kafkaproducerconsumer/config/KafkaConfig.java
@@ -1,7 +1,15 @@
 package com.hendisantika.kafkaproducerconsumer.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +23,14 @@ import org.springframework.kafka.annotation.EnableKafka;
 @Configuration
 @EnableKafka
 public class KafkaConfig {
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
 }
