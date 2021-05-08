359fab452608a7702fa067749ba64dcf05d5c536
Hendi Santika <hendisantika@gmail.com>
2021-05-09 06:48:48 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/kafkaproducerconsumer/SpringbootKafkaProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.kafkaproducerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProducerConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaProducerConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/kafkaproducerconsumer/SpringbootKafkaProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.kafkaproducerconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProducerConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
