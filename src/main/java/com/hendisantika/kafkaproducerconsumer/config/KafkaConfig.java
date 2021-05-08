f3bb2fa191492abf1c7ec3db65f55e6d88cdcc7a
Hendi Santika <hendisantika@gmail.com>
2021-05-09 06:55:56 +0700

    #5 - Add KafkaConfig

---
+++ src/main/java/com/hendisantika/kafkaproducerconsumer/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.kafkaproducerconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kafka-producer-consumer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/21
+ * Time: 06.55
+ */
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
