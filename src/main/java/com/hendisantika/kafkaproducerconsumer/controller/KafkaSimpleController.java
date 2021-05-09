0686d64bc60e8b5c95a0d1d01e70e33d133bb656
Hendi Santika <hendisantika@gmail.com>
2021-05-09 07:00:57 +0700

    #11 - Add post Model API in KafkaSimpleController

--- src/main/java/com/hendisantika/kafkaproducerconsumer/controller/KafkaSimpleController.java
+++ src/main/java/com/hendisantika/kafkaproducerconsumer/controller/KafkaSimpleController.java
@@ -1,8 +1,11 @@
 package com.hendisantika.kafkaproducerconsumer.controller;
 
 import com.google.gson.Gson;
+import com.hendisantika.kafkaproducerconsumer.model.SimpleModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,10 @@ public class KafkaSimpleController {
     public KafkaSimpleController(KafkaTemplate<String, String> kafkaTemplate, Gson jsonConverter) {
         this.kafkaTemplate = kafkaTemplate;
         this.jsonConverter = jsonConverter;
+    }
+
+    @PostMapping
+    public void post(@RequestBody SimpleModel simpleModel) {
+        kafkaTemplate.send("myTopic", jsonConverter.toJson(simpleModel));
     }
 }
